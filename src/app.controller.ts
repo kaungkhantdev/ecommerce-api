2127282a15d4277fc0c888337a4c29a5f64f31c9
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2025-12-07 15:25:39 +0700

    setup

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('data')
+  getData(): {
+    data: {
+      id: number;
+      name: string;
+    };
+    metadata: {
+      total: number;
+      next_page: number;
+      size: number;
+    };
+  } {
+    return this.appService.getData();
+  }
+}
